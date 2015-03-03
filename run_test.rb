c5ed540e0e0be79e9ac75a5243be327533ace00c
Tomoyuki Sahara <tsahara@iij.ad.jp>
2015-03-03 14:40:59 +0900

    mruby-require uses eval now.

--- run_test.rb
+++ run_test.rb
@@ -20,6 +20,7 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
 
+  conf.gem :core => 'mruby-eval'
   conf.gem :git => 'https://github.com/iij/mruby-io.git'
   conf.gem :git => 'https://github.com/iij/mruby-dir.git'
   conf.gem :git => 'https://github.com/iij/mruby-tempfile.git'
